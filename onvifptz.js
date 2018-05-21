013f7098f0566f5986806218aa57e8fc49f6cf27
bartbutenaers <bart.butenaers@gmail.com>
2018-05-21 08:42:11 +0200

    Added action list

--- onvifptz.js
+++ onvifptz.js
@@ -24,7 +24,7 @@
         this.zoomSpeed  = parseFloat(config.zoomSpeed);
         this.time       = parseInt(config.time);
         this.profile    = config.profile;
-        this.continuous = config.continuous;
+        this.action     = config.action;
         
         var node = this;
         
@@ -72,11 +72,22 @@
         }
                 
         node.on("input", function(msg) {
+            var newMsg = {};
+
             var panSpeed  = node.panSpeed;
             var tiltSpeed = node.tiltSpeed;
             var zoomSpeed = node.zoomSpeed;
             
-            if (!node.device.getCurrentProfile()) {
+            var action = node.action || msg.action;
+            
+            if (!action) {
+                console.warn('When no action specified in the node, it should be specified in the msg.action');
+                return;
+            }
+            
+            var currentProfile = node.device.getCurrentProfile();
+            
+            if (!currentProfile) {
                 // Avoid errors during ptzMove, by ensuring that the device has a profile.
                 // This can be a temporary issue at flow startup, since the device initialization above can take some time...
                 console.warn('Ignoring input message because the OnVif device has no current profile');
@@ -84,6 +95,7 @@
             }
             
             // Check whether the current PTZ movement should be interrupted
+            // TODO ipv msg.stop beter gewoon msg.action = stop
             if (msg.hasOwnProperty('stop') || msg.stop === true) {
                 node.device.ptzStop().catch((error) => {
                     console.error(error);
@@ -124,41 +136,147 @@
             // Make sure the values are between -1.0 and 1.0
             panSpeed  = Math.min(Math.max(panSpeed, -1.0), 1.0);
             tiltSpeed = Math.min(Math.max(tiltSpeed, -1.0), 1.0);
-            zoomSpeed = Math.min(Math.max(zoomSpeed, -1.0), 1.0);       
-         
-            if (node.continuous) {
-                var params = {
-                    'ProfileToken': node.profile,
-                    'Velocity': {
-                        x: panSpeed,
-                        y: tiltSpeed,
-                        z: zoomSpeed
-                    },
-                    'Timeout': node.time 
-                };
+            zoomSpeed = Math.min(Math.max(zoomSpeed, -1.0), 1.0);    
 
-                // Move the camera continuously 
-                node.device.services.ptz.continuousMove(params).catch((error) => {
-                    console.error(error);
-                });
-            }
-            else {
-                var params = {
-                    'speed': {
-                        x: panSpeed,
-                        y: tiltSpeed,
-                        z: zoomSpeed
-                    },
-                    'timeout': node.time
-                };
-                
-                // Move the camera once with the specified speed(s) and during the specified time
-                node.device.ptzMove(params).catch((error) => {
-                    console.error(error);
-                });
+            newMsg.xaddr = this.deviceConfig.xaddress;
+            newMsg.action = action;            
+         
+            switch (action) { 
+                case "move":
+                    var params = {
+                        'speed': {
+                            x: panSpeed,
+                            y: tiltSpeed,
+                            z: zoomSpeed
+                        },
+                        'timeout': node.time
+                    };
+                    
+                    // Move the camera once with the specified speed(s) and during the specified time
+                    node.device.ptzMove(params).catch((error) => {
+                        console.error(error);
+                    });
+                    
+                    break;
+                case "continuous":
+                    var params = {
+                        'ProfileToken': node.profile,
+                        'Velocity': {
+                            x: panSpeed,
+                            y: tiltSpeed,
+                            z: zoomSpeed
+                        },
+                        'Timeout': node.time 
+                    };
+
+                    // Move the camera continuously 
+                    node.device.services.ptz.continuousMove(params).catch((error) => {
+                        console.error(error);
+                    });
+                    
+                    break;
+                case "gotoHomePosition":
+                    var params = {
+                        'ProfileToken': currentProfile['token'],
+                        'Speed'       : 1 // TODO is this correct ???
+                    };
+                    
+                    // Let the camera go to the home position.
+                    // Make sure a home position is set in advance, otherwise you get a 'No HomePosition' error.
+                    node.device.services.ptz.gotoHomePosition(params).catch((error) => {
+                        console.error(error);
+                    });
+                    
+                    break;
+                case "setHomePosition":
+                    var params = {
+                        'ProfileToken': currentProfile['token'],
+                    };
+                    
+                    // Set the CURRENT camera position as the home position
+                    node.device.services.ptz.setHomePosition(params).catch((error) => {
+                        console.error(error);
+                    });
+                    
+                    break;
+                case "getPresets":
+                    var params = {
+                        'ProfileToken': currentProfile['token'],
+                    };
+                    
+                    node.device.services.ptz.getPresets(params).then((result) => {
+                        newMsg.payload = result.data.GetPresetsResponse.Preset;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break;
+                case "getNodes":
+                    node.device.services.ptz.getNodes().then((result) => {
+                        newMsg.payload = result.data.GetNodesResponse.PTZNode;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break;   
+                case "getNode":
+                    var params = {
+                        'NodeToken': 'PtzNode',
+                    };
+                    
+                    node.device.services.ptz.getNode(params).then((result) => {
+                        newMsg.payload = result.data.GetNodeResponse.PTZNode;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break;    
+                case "getConfigurations":
+                    node.device.services.ptz.getConfigurations().then((result) => {
+                        newMsg.payload = result.data.GetConfigurationsResponse.PTZConfiguration;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break; 
+                case "getConfiguration":
+                    var params = {
+                        'ConfigurationToken': 'PtzConf1',
+                    };
+                    
+                    node.device.services.ptz.getConfiguration(params).then((result) => {
+                        newMsg.payload = result.data.GetConfigurationResponse.PTZConfiguration;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break;     
+                case "getConfigurationOptions":
+                    var params = {
+                        'ConfigurationToken': 'PtzConf1',
+                    };
+                    
+                    node.device.services.ptz.getConfigurationOptions(params).then((result) => {
+                        newMsg.payload = result.data.GetConfigurationOptionsResponse.PTZConfigurationOptions;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break;        
+                case "getStatus":
+                    var params = {
+                        'ProfileToken': currentProfile['token'],
+                    };
+                    
+                    node.device.services.ptz.getStatus(params).then((result) => {
+                        newMsg.payload = result.data.GetStatusResponse.PTZStatus;
+                        node.send(newMsg);
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                    break;                       
             }
         });
     }
     RED.nodes.registerType("onvifptz",OnVifPtzNode);
 }
- 
